4ef3825748dc0d1b3a49416c43fdf6c9a0ef76c2
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-27 19:07:32 -0800

    Successfully scraped craigslist. Now going to format the output

--- craigslist_jobs/craigslist_jobs/spiders/jobs.py
+++ craigslist_jobs/craigslist_jobs/spiders/jobs.py
@@ -4,8 +4,18 @@ import scrapy
 
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
-    allowed_domains = ['https://lasvegas.craigslist.org/search/jjj?query=developer']
-    start_urls = ['http://https://lasvegas.craigslist.org/search/jjj?query=developer/']
+    allowed_domains = ['craigslist.org']
+    start_urls = ['https://lasvegas.craigslist.org/search/sof']
 
     def parse(self, response):
-        pass
+    	# xpath is how the script will extract portions of text with a set of rules to follow
+    	# for instance in the next line
+    	# `//` means instead of starting from the <html> tag, just start from the tag that is specified after it
+    	# in this intance, /a would mean <a> tag
+    	# the `[@class="result-title hdrlink"]` means that the <a> tag must have the class name of title
+    	# text() refers to the text of the <a> tag 
+    	# extact() extracts every instance on the web page that follows these xpath rules
+    	# if extract_first() is used instead of extract(), then only the first instance that follows the xpath will be extracted
+    	titles = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+    	print(titles)
+
